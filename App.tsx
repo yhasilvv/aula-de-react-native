07e358f827d362495e4ddab44439746f88023b3a
yhasmin <yhassilvv@gmail.com>
2025-11-27 19:42:56 -0300

    refactor: ajusta config tailwind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { ScreenContent } from '@/components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { LoginScreen } from '@/screens/login';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <SafeAreaView className='flex-1'>
+        <LoginScreen />
+      </SafeAreaView>
       <StatusBar style="auto" />
     </>
   );
