66fadaeaab4fc01484f901aec01f34332aeb35ad
yhasmin <yhassilvv@gmail.com>
2025-12-02 20:49:25 -0300

    feat: implementar tela de login com imput customizados

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,15 +1,24 @@
 import { BotaoCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
 
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  console.log("login", login );
+  console.log("senha", senha );
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <InputCustomizado placeholder={"Login"} />
-      <InputCustomizado placeholder={"Senha"} />
-      <BotaoCustomizado title="Entrar" />
-
-    </View>
+    <View className="flex-1 items-center justify-center ">
+      <View className="w-full p-5 gap-5 justify-center items-center">
+        <Text className="text-3xl">Nosso app</Text>
+        <InputCustomizado placeholder={'Login'} onChangeText={(text) => setLogin(text)} />
+        <InputCustomizado placeholder={'Senha'} secureTextEntry={true} onChangeText={(text) => setSenha(text)} />
+        <BotaoCustomizado title="Entrar" onPress={() => console. log ("login" , login, "senha", senha)}/>
+        
+        <Text className=" text to-blue-600">Esqueceu a senha?</Text>
+      </View>
+    </View> 
   );
 }
